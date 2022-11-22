d5fcc4b5fc2b0aeb3fbd50a48ea15d246ad35ac1
RybkinAD <115185954+RybkinAD@users.noreply.github.com>
2022-11-22 16:32:40 +0300

    Update task_4.py

--- Лабораторная работа 6/task_4.py
+++ Лабораторная работа 6/task_4.py
@@ -3,20 +3,15 @@ import json
 INPUT_FILE = "input.csv"
 
 
-def csv_to_list_dict(filename, delimeter=',', new_line='\n') -> list[dict]:
+def csv_to_list_dict(filename, delimeter=',') -> list[dict]:
     with open(filename, 'r') as f:
+        header_list = f.readline().rstrip('\n').split(',')
         read_list = f.read().split()
-        header_list = read_list[0].split(delimeter)
-        data = read_list[1:]
         data_list = []
-        for i in data:
-            data_list.append(list(data[data.index(i)].split(delimeter)))
-        list_dict = []
-        for j in range(len(data_list)):
-            each_dict = {}
-            list_dict.append(each_dict)
-            for k in range(len(header_list)):
-                each_dict[header_list[k]] = data_list[j][k]
+        for i in read_list:
+            data_list.append(read_list[read_list.index(i)].split(delimeter))
+        list_dict = [{header_list[j]: data_list[k][j] for j in range(len(header_list))}
+                     for k in range(len(data_list))]
         return list_dict
 
 
